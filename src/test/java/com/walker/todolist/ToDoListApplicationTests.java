3c21d8f9a003f3e39a60f533bcb413ec28a17dda
Walker Negrao <1882452@tcs.com>
2023-12-06 10:00:43 -0300

    Criação da Aplicação

---
+++ src/test/java/com/walker/todolist/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walker.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
